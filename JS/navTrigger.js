f54953c26e805472f63a67cec36262ef8c44c828
Noah Steiniger <noah.steiniger@epitech.eu>
2023-12-14 11:02:50 +0100

    feat (ALL) : navbar working with animation

--- JS/navTrigger.js
+++ JS/navTrigger.js
@@ -17,17 +17,17 @@ async function triggerNav() {
     while (newRight !== -31) {
       newRight--;
       nav.style.right = newRight + 'vw';
-      await delay(0.2);
+      await delay(0.1);
     }
   } else {
     newRight = -31;
     nav.classList.remove('inactive');
     nav.classList.add('active');
-    hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 64"><line x1="9.37" x2="54.63" y1="9.37" y2="54.63" fill="none" stroke="#010101" stroke-miterlimit="10" stroke-width="4"/><line x1="9.37" x2="54.63" y1="54.63" y2="9.37" fill="none" stroke="#010101" stroke-miterlimit="10" stroke-width="4"/></svg>`;
+    hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 64"><line x1="9.37" x2="54.63" y1="9.37" y2="54.63" fill="#FFFFFF" stroke="#FFFFFF" stroke-miterlimit="10" stroke-width="4"/><line x1="9.37" x2="54.63" y1="54.63" y2="9.37" fill="#FFFFFF" stroke="#FFFFFF" stroke-miterlimit="10" stroke-width="4"/></svg>`;
     while (newRight != 0) {
       newRight++;
       nav.style.right = newRight + 'vw';
-      await delay(0.2);
+      await delay(0.1);
     }
   }
 }
