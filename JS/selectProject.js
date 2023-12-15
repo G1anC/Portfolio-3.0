ff9b9864cd89b2f361aa83954d4876e23c32d5fc
Noah Steiniger <noah.steiniger@epitech.eu>
2023-12-15 10:51:47 +0100

    feat (PROJECTS) : add of onclick effect on selectors

--- JS/selectProject.js
+++ JS/selectProject.js
@@ -1,3 +1,8 @@
+const projects = document.getElementsByClassName("project");
+const selectors = document.getElementsByClassName("selector");
+const img = document.getElementsByTagName("img");
+let index = 0;
+
 function delay(milliseconds)
 {
     return new Promise((resolve) => {
@@ -5,26 +10,30 @@ function delay(milliseconds)
     });
 }
 
-const projects = document.getElementsByClassName("project");
+Array.from(projects).forEach((element) => { element.style.display = "none" })
 
-Array.from(projects).forEach((element) => {
-    element.style.display = "none"
-})
-
-Array.from(document.getElementsByClassName("selector")).forEach((element) => {
+Array.from(selectors).forEach((element) => {
     element.addEventListener("mouseenter", async (event) => {
-        let i = 0
-        let index = event.target.dataset.value
-        projects[index].style.display = "block"
-        while (i < 100) {
-            projects[index].style.opacity = i + "%"
-            i += 3
-            await delay(0.05)
-        }
-    })
-    element.addEventListener("mouseleave", async (event) => {
         index = event.target.dataset.value
+        projects[index].style.display = "block"
+        for (let i = 0; i < 71; i += 5, await delay(0.05))
+            projects[index].style.opacity = i + "%"
+    })
+    element.addEventListener("mouseleave", () => {
         projects[index].style.display = "none"
         projects[index].style.opacity = "0%"
+    })
+    element.addEventListener("click", async () => {
+    // i need to display the project as larger and with an opacity of 100% without the blur
+    // and drop the opacity of the selectors to 0%
+        let blur = 20
+        const projectsWrapper = document.getElementById("projects-wrapper")
+        for (let i = 70; i < 100; i += 5, await delay(0.05)) {
+            projects[index].style.opacity = i + "%"
+            if (blur > 0) blur -= 2
+            img[index].style.filter = "blur(" + blur + "px)"
+        }
+        projectsWrapper.style.transfrom = "scale(1.2)";
+        
     })
 })
