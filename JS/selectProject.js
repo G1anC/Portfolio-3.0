3efe499177626cfbd51a640fc610f008302ef601
Noah Steiniger <noah.steiniger@epitech.eu>
2023-12-15 11:43:17 +0100

    feat (PROJECTS) : onclick | mouseleave | mouseenter properly working

--- JS/selectProject.js
+++ JS/selectProject.js
@@ -1,8 +1,10 @@
-const projects = document.getElementsByClassName("project");
-const selectors = document.getElementsByClassName("selector");
-const img = document.getElementsByClassName("project-image");
+const projects = document.getElementsByClassName("project")
+const selectors = document.getElementsByClassName("selector")
+const img = document.getElementsByClassName("project-image")
+const text = document.getElementsByClassName("project-text")
+const projectsWrapper = document.getElementById("projects-wrapper")
+
 let index = 0;
-let stay = false;
 
 function delay(milliseconds)
 {
@@ -11,39 +13,31 @@ function delay(milliseconds)
     });
 }
 
-async function enterProject(event) {
-    index = event.target.dataset.value
+async function enterProject(e) {
+    index = e.target.dataset.value
     projects[index].style.display = "block"
     img[index].style.filter = "blur(20px)"
+    text[index].style.opacity = "100%";
     for (let i = 0; i < 71; i += 5, await delay(0.05))
         projects[index].style.opacity = i + "%"
-    stay = false;
 }
 
 function unDisplayProject() {
-    if (stay === false) {
-        projects[index].style.display = "none"
-        projects[index].style.opacity = "0%"
-    }
+    projects[index].style.display = "none"
+    projects[index].style.opacity = "0%"
 }
 
-async function showEntireProject() {
-    let blur = 20
-    const projectsWrapper = document.getElementById("projects-wrapper")
-    for (let i = 70; i < 100; i += 5, await delay(0.05)) {
-        projects[index].style.opacity = i + "%"
-        if (blur > 0)
-            blur -= 2
-        img[index].style.filter = "blur(0px)"
-    }
-    stay = true;
+function showEntireProject() {
+    img[index].style.filter = "blur(0px)"
+    projects[index].style.opacity = "100%";
     projectsWrapper.style.transfrom = "scale(1.2)";
+    text[index].style.opacity = "0";
 }
 
 Array.from(projects).forEach(element => { element.style.display = "none" })
 
 Array.from(selectors).forEach(element => {
-    element.addEventListener("mouseenter", event => {enterProject(event)})  
-    element.addEventListener("mouseleave", unDisplayProject())
-    element.addEventListener("click", showEntireProject())
+    element.addEventListener("mouseenter",  e => { enterProject(e) })  
+    element.addEventListener("click",      () => { showEntireProject() })
+    element.addEventListener("mouseleave", () => { unDisplayProject() })
 })
